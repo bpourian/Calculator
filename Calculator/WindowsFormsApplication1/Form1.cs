15ff75911039d567caee8e0bfe6ae7ed83707f2d
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-01-19 23:11:23 +0000

    bpourian
    bpourian project zero

---
+++ Calculator/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        double value = 0;
+        string operation = "";
+        bool operation_pressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //numbers including the . 
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operation_pressed))
+                result.Clear();
+
+            operation_pressed = false;
+            Button b = (Button)sender;
+            result.Text = result.Text + b.Text;
+        }
+
+        //CE  clear everything button
+        private void button19_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+
+
+        //C  Just the clear button 
+        private void button18_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+            value = 0;
+        }
+
+
+
+        //operator i.e. +, -, /, *
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = b.Text;
+            value = double.Parse(result.Text);
+            operation_pressed = true;
+            equation.Text = value + " " + operation;
+            //result.Clear();
+        }
+
+
+        //eqaul button
+        private void button16_Click(object sender, EventArgs e)
+        {
+            equation.Text = "";
+            switch (operation)
+            {
+                case "+":
+                    result.Text = (value + double.Parse(result.Text)).ToString();
+                    break;
+                case "-":
+                    result.Text = (value - double.Parse(result.Text)).ToString();
+                    break;
+                case "*":
+                    result.Text = (value * double.Parse(result.Text)).ToString();
+                    break;
+                case "/":
+                    result.Text = (value / double.Parse(result.Text)).ToString();
+                    break;
+                default:
+                    break;
+                
+            }//end switch
+            
+            
+        }
+
+    }
+}
